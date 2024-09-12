e0e52143615922450de84711aefc828aaebd5660
jihen mansour <jihenmn8@gmail.com>
2024-09-12 13:48:50 +0200

    update destinantion path

--- backend/utils/multer.config.js
+++ backend/utils/multer.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-      cb(null, './tmp')
+      cb(null, __dirname + '/tmp')
     },
   
     filename: (req, file, cb) => {
