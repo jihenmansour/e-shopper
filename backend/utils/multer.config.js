6f4aececeeb3baa53c24b49ee2c1cb1ad61713ed
jihen mansour <jihenmn8@gmail.com>
2024-09-12 13:38:31 +0200

    update express.static method

--- backend/utils/multer.config.js
+++ backend/utils/multer.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-      cb(null, path.join(__dirname,"../public/images"));
+      cb(null, "public/images");
     },
   
     filename: (req, file, cb) => {
