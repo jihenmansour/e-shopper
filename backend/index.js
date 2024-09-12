1d03c2535d90cbf125419f2686a5aa92d8553920
jihen mansour <jihenmn8@gmail.com>
2024-09-12 13:19:16 +0200

    update path of uploading images

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
+const path = require("path");
 require("express-async-errors")
 
 const authRoutes = require('./routes/auth.router')
@@ -20,7 +21,7 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.json())
 app.use(cors("*"))
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname,"public/images")));
 
 try {
      mongoose.connect(process.env.MONGODB_CONNECT_URL)
